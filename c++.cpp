afc813412c50f14f89c95f618d0a38087a9d8c65
Tommy Vo <1923847@edu.vaniercollege.qc.ca>
2024-02-05 12:48:44 -0500

    saved

---
+++ c++.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() { // The { introduces the function. It is the beginning of block for "main"
+    // This "int main ( )" thing is the introduction of a function
+    // Second comment
+    /* This
+    is
+    a
+    multi-line comment */
+    
+    cout << "Hello world!"; //Output statement. Must include double quotations between whatever is to be printed (shown).
+
+    return 0; //
+} //Concludes the function. It is the end of block for "main"
+
+int main () {
+
+    cout << "Second paragraph";
+
+    return 0;
+}
